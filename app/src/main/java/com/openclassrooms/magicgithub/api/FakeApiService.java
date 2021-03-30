9eee2c785ffed92d8ba3c7177d608058a5d99fd4
cjanie <janiechun@hotmail.fr>
2021-03-30 17:17:35 +0200

    all tests succeed

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -25,7 +25,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        this.users.add(FAKE_USERS_RANDOM.get((int) (Math.random() * FAKE_USERS_RANDOM.size() - 1)));
+        this.users.add(FAKE_USERS_RANDOM.get((int) (Math.random() * FAKE_USERS_RANDOM.size())));
     }
 
     /**
